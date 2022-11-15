e4bcc6422949f01a16c488c97f858d933da60954
Pinksharkapps <pinksharkapps@gmail.com>
2022-11-15 22:05:09 +0300

    hope i'm commiting the working hangman from lesson

--- UD05_hangman/Program.cs
+++ UD05_hangman/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace UD05_hangman
+﻿using System; 
+using System.IO;
+
+namespace UD05_hangman
 {
     internal class Program
     {
+        private static string path = @"/Users/ascha/Unity_Projects/UD05_hangman/UD05_hangman/word_rus.txt";
         public static void Main(string[] args)
         {
+           string[] words = File.readAllLines(path); //прочитает все строки из файла и засунет в массив
+           Console.WriteLine(words.Length);
+
+
+           //Console.WriteLine(" Hello World ");
+           //Console.ReadLine();
         }
     }
 }
