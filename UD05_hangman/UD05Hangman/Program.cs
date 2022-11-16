36b363119e0da2cb07f4793af7e3c55acb9fc07c
HOME-PC\JUST <roman.raven96@mail.ru>
2022-11-16 16:59:40 +0300

    Update gitignore, add script code

---
+++ UD05_hangman/UD05Hangman/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+
+namespace UD05Hangman
+{
+    internal class Program
+    {
+        private static string Path = @"D:\Project Hangman\UD05_hangman\word_rus.txt";
+        private const int maxErrors = 10;
+
+        public static void Main()
+        {
+            //считываем словарь
+            string[] words = File.ReadAllLines(Path);
+
+            Console.WriteLine("Добро пожаловать в игру!");
+            while (true)
+            {
+                //загадываем слово
+                Random random = new Random();
+                string word = words[random.Next(0, words.Length)];
+                Console.WriteLine(word);
+                char[] charWord = word.ToCharArray();
+                Console.WriteLine(charWord.Length);
+
+                //создаём счётчик
+                int openedLetters = 0;
+                int errors = maxErrors;
+
+                //формируем строку для отображения процесса
+                char[] viewWord = new char[charWord.Length];
+
+                for (int i = 0; i < viewWord.Length; i++)
+                {
+                    viewWord[i] = '*';
+                }
+
+                Console.WriteLine(new string(viewWord));
+
+                Console.WriteLine($"Я загадал слово из {charWord.Length} букв");
+
+                while (errors > 0 && openedLetters != charWord.Length)
+                {
+                    //просим ввести букву и считываем её
+                    Console.WriteLine("Введите букву");
+                    string inputString = Console.ReadLine();
+
+                    //проверяем введённые данные
+                    if (inputString.Length == 0 || !Char.IsLetter(inputString[0]))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Вводите только буквы!");
+                    }
+
+                    //проверяем есть ли такая буква в слове
+                    char letter = inputString[0];
+                    bool isLetterExist = false;
+                    for (int i = 0; i < charWord.Length; i++)
+                    {
+                        if (charWord[i] == letter)
+                        {
+                            openedLetters++;
+                            viewWord[i] = charWord[i];
+                            charWord[i] = '-';
+                            isLetterExist = true;
+                        }
+                    }
+
+                    Console.Clear();
+                    if (isLetterExist)
+                    {
+                        Console.WriteLine("Верно!");
+                    }
+                    else
+                    {
+                        errors--;
+                        Console.WriteLine($"Такой буквы нет! Осталось {errors} попыток");
+                    }
+                    Console.WriteLine(new string(viewWord));
+                }
+                
+                if (errors == 0)
+                {
+                    Console.WriteLine($"Попытки закончились! Ты проиграл! Искомое слово - {word}");
+                }
+                else
+                {
+                    Console.WriteLine("Все буквы угаданы верно! Победа!");
+                }
+
+                Console.WriteLine("Для продолжения нажми Enter");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
--- UD05_hangman/UD05Hangman/Properties/AssemblyInfo.cs
+++ UD05_hangman/UD05Hangman/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UD05_hangman")]
+[assembly: AssemblyTitle("UD05Hangman")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UD05_hangman")]
+[assembly: AssemblyProduct("UD05Hangman")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("FF0ACA54-EC24-4B89-BF3E-75614E136F54")]
+[assembly: Guid("A75D4202-958B-48D8-8F8C-8FD3AE5836F1")]
 
 // Version information for an assembly consists of the following four values:
 //
