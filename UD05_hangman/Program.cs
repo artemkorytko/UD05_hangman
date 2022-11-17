f8ee68a90916172c4d6afb44b2460f08218fc0f5
YanaKryveleva <yana.kryveleva@gmail.com>
2022-11-17 19:05:39 +0300

    commit

--- UD05_hangman/Program.cs
+++ UD05_hangman/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace UD05_hangman
+﻿using System;
+
+namespace UD05_hangman
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
