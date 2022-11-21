071c3497953041d6e7f00082c27cbf1ba69f4876
Teistail <teistail@gmail.com>
2022-11-21 22:46:35 +0300

    homwork calculator

---
+++ calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int x = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите второе число");
+            int y = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите символ");
+            string symbol = Console.ReadLine();
+
+            int result = 0;
+
+            if (symbol == "+")
+            {
+                result = x + y;
+            }
+            else if (symbol == "-")
+            {
+                result = x - y;
+            }
+            else if (symbol == "*")
+            {
+                result = x * y;
+            }
+            else if (symbol == "/")
+            {
+                result = x / y;
+            }
+            else if (symbol == "^")
+            {
+                result = x ^ y;
+            }
+            
+            Console.WriteLine("Результат=" + result);
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+    }
+}
+    
