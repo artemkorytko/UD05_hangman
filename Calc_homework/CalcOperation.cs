e82004d800aa2c392c02b5801ac698b06342a8a1
Frenchman92 <frenchman9218@gmail.com>
2022-11-21 20:38:33 +0300

    add calculator

---
+++ Calc_homework/CalcOperation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calc_homework
+{
+    public class CalcOperation
+    {
+        private int _x;
+        private int _y;
+        private char _sing;
+
+        public CalcOperation(int x, int y, char sing)
+        {
+            _x = x;
+            _y = y;
+            _sing = _sing;
+        }
+
+        public string WriteResult()
+        {
+            double result = 0.0;
+            switch (_sing)
+            {
+                case '+':
+                    result = _x + _y;
+                    break;
+
+                case '-':
+                    result = _x - _y;
+                    break;
+                
+                case '/':
+                    if (_y != 0)
+                        result = Convert.ToDouble(_x / _y);
+                    else
+                        return "Деление на ноль невозможно!";
+                    break;
+
+                case '*':
+                    result = _x * _y;
+                    break;
+
+                case '^':
+                    result = Math.Pow(_x, _y);
+                    break;
+
+                default:
+                    Console.WriteLine("Неверный оператор");
+                    break;
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Calc_homework/Cals.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calc_homework
+{
+    internal class Cals
+    {
+        private static char[] chars = { '+', '-', '*', '/', '^'};
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Что нужно посчитать?: ");
+                string inputData = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(inputData) && CheckInputData(inputData))
+                {
+                    string[] str = inputData.Split(chars, 2);
+                    int x = CheckParse(str[0]);
+                    int y = CheckParse(str[1]);
+
+                    CalcOperation process = new CalcOperation(x, y, SignSearch(inputData));
+                    Console.WriteLine(process.WriteResult());
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Введите корректное выражение.");
+                }
+            }
+        }
+
+
+        private static char SignSearch(string str)
+        {
+            int index = -1;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (index == -1)
+                    index = str.IndexOf(chars[i]);
+            }
+
+            return str[index];
+        }
+
+        private static bool CheckInputData(string data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (Char.IsLetter(data[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static int CheckParse(string str)
+        {
+            int number;
+            bool isCheck = int.TryParse(str, out number);
+
+            if (isCheck)
+                return Int32.Parse(str);
+
+            Console.WriteLine("Введите корректное выражени!, return 0");
+            return 0;
+        }
+    }
+}
