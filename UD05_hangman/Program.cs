a04a9797aa29185cb939f33a27eee5665aade410
DESKTOP-7MGTNMM\Kirill <kirillianoreva@gmail.com>
2022-11-21 22:19:54 +0300

    calculate

--- UD05_hangman/Program.cs
+++ UD05_hangman/Program.cs
@@ -1,9 +1,62 @@
-﻿namespace UD05_hangman
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UD05_hangman
 {
-    internal class Program
+    internal class Calculator
     {
         public static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+                double x, y;
+                string deistvie;
+
+                try
+                {
+                    x = Double.Parse(Console.ReadLine());
+                    y = Double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите число!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+               
+
+                Console.WriteLine("Выберите действие: '+' '-' '/' '*' ");
+                deistvie = Console.ReadLine();
+
+                switch (deistvie)
+                {
+                    case "+":
+                        Console.WriteLine(x + y);
+                        break;
+                    case "-":
+                        Console.WriteLine(x - y);
+                        break;
+                    case "*":
+                        Console.WriteLine(x * y);
+                        break;
+                    case "/":
+                        if (y == 0)
+                            Console.WriteLine(0);
+                        else
+                        {
+                            Console.WriteLine(x / y);
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("Введите верное действие");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
