ceb7dfb06f45fcd48bf5cd269fc2e6eeed0845f7
HOME-PC\JUST <roman.raven96@mail.ru>
2022-11-21 16:52:32 +0300

    Add MyCalc project (homework)

---
+++ UD05_hangman/MyCalc/CalcLogic.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace MyCalc
+{
+    public class CalcLogic
+    {
+        private int _x;
+        private int _y;
+        private char _sign;
+        public int _result;
+        public string _error;
+
+        //создаём конструктор
+        public CalcLogic(int x, int y, char sign)
+        {
+            _x = x;
+            _y = y;
+            _sign = sign;
+        }
+
+        public string CheckDivNull()
+        {
+            if (_sign == '/' && _y == 0)
+            {
+                _error = "На ноль делить нельзя!";
+            }
+
+            return _error;
+        }
+
+        public int Calculation()
+        {
+            switch (_sign)
+            {
+                case '+':
+                    _result = _x + _y;
+                    break;
+                case '-':
+                    _result = _x - _y;
+                    break;
+                case '*':
+                    _result = _x * _y;
+                    break;
+                case '/':
+                    _result = _x / _y;
+                    break;
+                case '^':
+                    _result = Convert.ToInt32(Math.Pow(_x, _y));
+                    break;
+            }
+
+            return _result;
+        }
+    }
+}
---
+++ UD05_hangman/MyCalc/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace MyCalc
+{
+    internal class Program
+    {
+        private static string someString;
+        private static int x;
+        private static int y;
+        private static char sign;
+        private static string error;
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                //Просим ввести выражение для решения и считываем его
+                Console.WriteLine(
+                    "Введите выражение для решения и нажмите Enter. Каждая переменная и знак должны быть разделены пробелами!\nИспользуйте только целые числа! Например: 2421 + 4537");
+                someString = Console.ReadLine();
+
+                //переводим выражение в массив строк, преобразуем тип и выполняем проверки валидности данных
+                error = null;
+                string[] splitString = someString.Split(' ');
+                if (splitString.Length == 3)
+                {
+                    bool result1 = int.TryParse(splitString[0], out x);
+                    if (result1 == true)
+                    {
+                        //Console.WriteLine($"Успех! Первое число {x}"); //Debug message
+                        bool result2 = int.TryParse(splitString[2], out y);
+                        if (result2 == true)
+                        {
+                            //Console.WriteLine($"Успех! Второе число {y}"); //Debug message
+                            bool result3 = char.TryParse(splitString[1], out sign);
+                            if (result3 == true && sign == '+' || sign == '-' || sign == '*' || sign == '/' ||
+                                sign == '^')
+                            {
+                                //Console.WriteLine($"Успех! Знак {sign}"); //Debug message
+                            }
+                            else
+                            {
+                                error = "Неверный формат введённого выражения. Не введён знак арифметической операции!";
+                            }
+                        }
+                        else
+                        {
+                            error =
+                                "Неверный формат введённого выражения. В качестве второй переменной введены неверные данные!";
+                        }
+                    }
+                    else
+                    {
+                        error = "Неверный формат введённого выражения. В качестве первой переменной введены неверные данные!";
+                    }
+                }
+                else
+                {
+                    error =
+                        "Неверный формат введённого выражения. Каждая переменная и знак должны быть разделены пробелами!";
+                }
+
+                //Инициализируем экземпляр класса и выполняем проверку деления на 0
+                CalcLogic Calc = new CalcLogic(x, y, sign);
+                if (error == null)
+                {
+                    error = Calc.CheckDivNull();
+                }
+
+                //выводим ответ, либо сообщение об ошибке
+                if (error == null)
+                {
+                    Console.WriteLine($"{someString} = {Calc.Calculation()}");
+                }
+                else
+                {
+                    Console.WriteLine(error);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Для решения следующего выражения нажмите Enter");
+                Console.ReadLine();
+            }
+        }
+    }
+}
