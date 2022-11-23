d4f586872adf7fd2188f80a8f703f63aef4a34b9
Darya <joewalker6999@gmail.com>
2022-11-23 15:27:37 +0300

    calculator[2]

---
+++ UD05_calculator/UD05_calculator/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace UD05_calculator
+{
+    public class Calculator
+    {
+        private int x;
+        private int y;
+        private char oper;
+        private int result = 0;
+
+        public int Calculate(string actualText)
+        {
+            string[] formattedText = actualText.Split(' ');
+            bool bIsValid = isValid(formattedText);
+            if (!bIsValid)
+            {
+                Console.WriteLine("Error");
+                return -1;
+            }
+
+            x = int.Parse(formattedText[0]);
+            oper = Convert.ToChar(formattedText[1]);
+            y = int.Parse(formattedText[2]);
+
+            return Calculation();
+        }
+
+        private bool isValid(string[] text)
+        {
+            for (int i = 0; i < 2; i += 2)
+            {
+                foreach (char ch in text[i])
+                {
+                    if (!char.IsDigit(ch)) return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int Calculation()
+        {
+            if (oper == '+')
+            {
+                return x + y;
+            }
+
+            if (oper == '-')
+            {
+                return x - y;
+            }
+
+            if (oper == '*')
+            {
+                return x * y;
+            }
+
+            if (oper == '/')
+            {
+                return x / y;
+            }
+
+            if (oper == '^')
+            {
+                return Convert.ToInt32(Math.Pow(x, y));
+            }
+
+            Console.WriteLine("Что-то не так :c Проверьте правильность ввода данных.");
+            return -1;
+        }
+    }
+}
---
+++ UD05_calculator/UD05_calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace UD05_calculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.GetEncoding(1251);
+
+
+            Calculator calculator = new Calculator();
+            string actualText = Console.ReadLine();
+            int answer = calculator.Calculate(actualText);
+
+            Console.WriteLine($"Ответ: {answer}");
+        }
+    }
+}
